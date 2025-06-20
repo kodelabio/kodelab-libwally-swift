6bfdf48753cf256e445b37020f34d676c2c64cfb
Aden Eley <aden@kodelab.io>
2025-06-20 21:13:15 +0100

    Ensure we output dSYM symbols

--- build-libwally-swift.sh
+++ build-libwally-swift.sh
@@ -7,17 +7,24 @@ rm -rf $BIN_OUTPUT_DIRECTORY
 
 git submodule update --init --recursive
 
+# — iOS Simulator slice (arm64 + x86_64) —
 xcodebuild archive -scheme LibWally \
   -destination "generic/platform=iOS Simulator" \
   -archivePath ${BIN_OUTPUT_DIRECTORY}/LibWally-Sim \
-  SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+  SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+  DEBUG_INFORMATION_FORMAT=dwarf-with-dsym
 
+
+# — iOS device slice (arm64) —
 xcodebuild archive -scheme LibWally \
   -destination "generic/platform=iOS" \
   -archivePath ${BIN_OUTPUT_DIRECTORY}/LibWally-iOS \
-  SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+  SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+  DEBUG_INFORMATION_FORMAT=dwarf-with-dsym
 
 xcodebuild -create-xcframework \
-  -framework ${BIN_OUTPUT_DIRECTORY}/LibWally-iOS.xcarchive/Products/Library/Frameworks/LibWally.framework \
-  -framework ${BIN_OUTPUT_DIRECTORY}/LibWally-Sim.xcarchive/Products/Library/Frameworks/LibWally.framework \
-  -output ${BIN_OUTPUT_DIRECTORY}/LibWally.xcframework
+  -framework  "${BIN_OUTPUT_DIRECTORY}/LibWally-iOS.xcarchive/Products/Library/Frameworks/LibWally.framework" \
+  -debug-symbols "${BIN_OUTPUT_DIRECTORY}/LibWally-iOS.xcarchive/dSYMs/LibWally.framework.dSYM" \
+  -framework  "${BIN_OUTPUT_DIRECTORY}/LibWally-Sim.xcarchive/Products/Library/Frameworks/LibWally.framework" \
+  -debug-symbols "${BIN_OUTPUT_DIRECTORY}/LibWally-Sim.xcarchive/dSYMs/LibWally.framework.dSYM" \
+  -output "${BIN_OUTPUT_DIRECTORY}/LibWally.xcframework"
